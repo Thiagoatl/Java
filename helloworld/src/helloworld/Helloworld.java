74abe939dbb47b333c3b40bf0bf2840efa33af69
Thiago Trevisani <Thiagoatkl@gmail.com>
2023-03-14 11:52:27 -0300

    Projeto Hello World

---
+++ helloworld/src/helloworld/Helloworld.java
@@ -0,0 +1,14 @@
+package helloworld;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int test = 10;
+		System.out.println("Hello World!");
+		System.out.println("Como vcs tão?");
+		System.out.println("teste variavel: " + test);
+	}
+
+}

f7cacac7b690f7e7994645f57fe4d1410d447855
Thiago Trevisani <Thiagoatkl@gmail.com>
2023-03-28 20:33:23 -0300

    Conta Repository

--- Projeto_conta_bancaria/src/conta/repository/ContaRepository.java
+++ Projeto_conta_bancaria/src/conta/repository/ContaRepository.java
@@ -1,5 +1,21 @@
 package conta.repository;
 
+import conta.model.Conta;
+
 public interface ContaRepository {
 
+	// CRUD de Conta
+		public void procurarPorNumero(int numero);
+		public void listarTodas();
+		public void cadastrar(Conta conta);
+		public void atualizar(Conta conta);
+		public void deletar(int numero);
+		
+		// Métodos Bancários
+		public void sacar(int numero, float valor);
+		public void depositar(int numero, float valor);
+		public void transferir(int numeroOrigem, int numeroDestino, float valor);
+	
+	
+	
 }
